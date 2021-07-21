2d2bab501121a71daf107b87073a762626b08e32
michaellaros <michael_larosa@studenti.naba.it>
2021-07-21 02:48:16 +0200

    turret

---
+++ Assets/turret/BulletForTurret.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletForTurret : MonoBehaviour
+{
+    public float movementSpeed;
+    private GameObject target;
+    public int damage;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     transform.Translate(Vector3.forward * Time.deltaTime * movementSpeed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Enemy")
+        {
+            target = other.gameObject;
+            target.GetComponent<Enemyscript>().health -= damage;
+            Destroy(this.gameObject);
+        }
+    }
+
+}
---
+++ Assets/turret/Turret.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret : MonoBehaviour
+{
+    public GameObject bullet;
+    private GameObject target;
+    private bool targetlocked;
+    public GameObject turretTopPart;
+    public float fireTimer;
+    private bool shootReady;
+    public GameObject bulletSpawnpoint;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        shootReady = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (targetlocked)
+        {
+            turretTopPart.transform.LookAt(target.transform);
+            turretTopPart.transform.Rotate(0, -90, 0);
+            if (shootReady)
+            {
+                Shoot();
+            }
+            
+        }
+    }
+    void Shoot()
+    {
+        Transform _bullet = Instantiate(bullet.transform, bulletSpawnpoint.transform.position, Quaternion.identity);
+        _bullet.transform.rotation = bulletSpawnpoint.transform.rotation;
+        shootReady = false;
+        StartCoroutine(FireRate());
+
+    }
+
+    IEnumerator FireRate()
+    {
+        yield return new WaitForSeconds(fireTimer);
+        shootReady = true;
+    }
+    
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag =="Enemy")
+        {
+            target = other.gameObject;
+            targetlocked = true;
+            if (targetlocked)
+                Debug.Log("colpito");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Enemy")
+        {
+            target = other.gameObject;
+            targetlocked = true;
+            
+                
+        }
+    }
+}

278f7a65b3e531e8191a8c0fd27915fad05a29b4
michaellaros <michael_larosa@studenti.naba.it>
2021-10-10 00:26:35 +0200

    turret

---
+++ Assets/Shooter.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : BNG.Grabbable
+{ public GameObject spawnPoint;
+    public GameObject Projectile;
+    public float launchVelocity = 500f;
+    public bool readyToShoot;
+    void Start()
+    {
+        readyToShoot = true;
+    }
+
+    // Update is called once per frame
+     override public void Update()
+    {
+        
+        
+            
+
+
+        if (BeingHeld == true && (input.RightTrigger > 0.1f) && readyToShoot)
+        {
+            var projectile = Instantiate(Projectile, spawnPoint.transform.position,Quaternion.identity);
+            projectile.GetComponent<Rigidbody>().AddRelativeForce(transform.forward * launchVelocity);
+            readyToShoot = false;
+            Invoke("ReReady", 2);
+
+            
+            
+            
+        }
+
+    }
+    public void ReReady()
+    {
+        readyToShoot = true;
+    }
+}

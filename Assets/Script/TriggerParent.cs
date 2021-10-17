b6587713f30ef1aaa22af1a4fef4c799880c12df
MarcoBondesaniNaba <Marco_Bondesani@studenti.naba.it>
2021-10-17 17:39:30 +0200

    player diventa figlio torrette

---
+++ Assets/Script/TriggerParent.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerParent : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.CompareTag("Player"))
+        {
+            collider.transform.SetParent(transform);
+        }
+    }
+    private void OnTriggerExit(Collider collider) {
+        if (collider.CompareTag("Player"))
+        {
+            collider.transform.SetParent(null);
+        }
+    }
+}

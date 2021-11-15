85bc9e0f513ddc73dcf8ad0e3926403514b50704
michaellaros <michael_larosa@studenti.naba.it>
2021-11-15 17:49:08 +0100

    ui fix

--- Assets/Script/Map_Trigger.cs
+++ Assets/Script/Map_Trigger.cs
@@ -5,21 +5,35 @@ using UnityEngine.UI;
 
 public class Map_Trigger : MonoBehaviour
 {
-    
+    public GameObject cameraMap;
     public bool mapOn;
     public GameObject map;
     // Start is called before the first frame update
+
+    private void Start()
+    {
+        map.SetActive(false);
+        cameraMap.SetActive(false);
+    }
     void Update()
     {
         if (mapOn)
+        {
             map.SetActive(true);
+            cameraMap.SetActive(true);
+        }
+
 
         if (!mapOn)
+        {
             map.SetActive(false);
-        
+            cameraMap.SetActive(false);
+
+        }
     }
 
-    public void ToggleMap()
+
+        public void ToggleMap()
     {
         mapOn = !mapOn;
 
